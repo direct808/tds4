a0705893f10a0794a54f3906338aac5cc3da7965
Larin Sergey <direct808@yandex.ru>
2022-11-29 19:32:40 +0300

    add many fields in offer

--- services/offer/src/dto/offer-save.DTO.ts
+++ services/offer/src/dto/offer-save.DTO.ts
@@ -1,13 +1,52 @@
-import { IsOptional, IsUUID } from 'class-validator'
+import { IsIn, IsOptional, IsString, IsUUID, ValidateIf } from 'class-validator'
+import { OfferActionType, OfferRedirectType, OfferType } from '../entities'
 
 export class OfferSaveDTO {
   @IsUUID('4')
   @IsOptional()
-  id!: string
+  id?: string
 
+  @IsString()
   name!: string
 
   @IsUUID('4')
   @IsOptional()
-  declare affiliateNetworkId: string
+  affiliateNetworkId?: string | null
+
+  @IsIn(Object.values(OfferType))
+  type!: OfferType
+
+  @ValidateIf((data: OfferSaveDTO) => data.type === OfferType.local)
+  @IsString()
+  file?: string
+
+  @ValidateIf((data: OfferSaveDTO) => data.type === OfferType.redirect)
+  @IsIn(Object.values(OfferRedirectType))
+  redirectType?: OfferRedirectType
+
+  @ValidateIf((data: OfferSaveDTO) => data.type === OfferType.redirect)
+  @IsString()
+  redirectUrl?: string
+
+  @ValidateIf((data: OfferSaveDTO) => data.type === OfferType.preload)
+  @IsString()
+  preloadUrl?: string
+
+  @ValidateIf((data: OfferSaveDTO) => data.type === OfferType.action)
+  @IsIn(Object.values(OfferActionType))
+  actionType?: OfferActionType
+
+  @ValidateIf(
+    (data: OfferSaveDTO) => data.actionType === OfferActionType.toCampaign,
+  )
+  @IsUUID('4')
+  actionCampaignId?: string
+
+  @ValidateIf((data: OfferSaveDTO) =>
+    [OfferActionType.showHtml, OfferActionType.showText].includes(
+      data.actionType!,
+    ),
+  )
+  @IsString()
+  actionContent?: string
 }
--- services/offer/src/entities/offer.entity.ts
+++ services/offer/src/entities/offer.entity.ts
@@ -1,5 +1,31 @@
 import { Column, Entity } from 'typeorm'
 
+export enum OfferType {
+  local = 'local',
+  redirect = 'redirect',
+  preload = 'preload',
+  action = 'action',
+}
+
+export enum OfferRedirectType {
+  http = 'http',
+  meta = 'meta',
+  js = 'js',
+  withoutReferer = 'withoutReferer',
+  curl = 'curl',
+  meta2 = 'meta2',
+  formSubmit = 'formSubmit',
+  iframe = 'iframe',
+  remote = 'remote',
+}
+export enum OfferActionType {
+  toCampaign = 'toCampaign',
+  show404 = 'show404',
+  showText = 'showText',
+  showHtml = 'showHtml',
+  nothing = 'nothing',
+}
+
 @Entity()
 export class Offer {
   @Column('uuid', { generated: 'uuid', primary: true })
@@ -10,4 +36,28 @@ export class Offer {
 
   @Column('uuid', { nullable: true })
   declare affiliateNetworkId: string | null
+
+  @Column('enum', { nullable: false, enum: OfferType })
+  declare type: OfferType
+
+  @Column('text', { nullable: true })
+  declare file: string | null
+
+  @Column('enum', { nullable: true, enum: OfferRedirectType })
+  declare redirectType: OfferRedirectType | null
+
+  @Column('text', { nullable: true })
+  declare redirectUrl: string | null
+
+  @Column('text', { nullable: true })
+  declare preloadUrl: string | null
+
+  @Column('enum', { nullable: true, enum: OfferActionType })
+  declare actionType: OfferActionType | null
+
+  @Column('uuid', { nullable: true })
+  declare actionCampaignId: string | null
+
+  @Column('text', { nullable: true })
+  declare actionContent: string | null
 }
