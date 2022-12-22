57c2dde34e345d3105f2b23eed1845fa8ef884b9
Larin Sergey <direct808@yandex.ru>
2022-12-22 10:59:54 +0300

    eslint-plugin-boundaries

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,14 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'boundaries'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:boundaries/recommended',
   ],
   root: true,
   env: {
@@ -21,5 +22,37 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'boundaries/element-types': [
+      2,
+      {
+        rules: [
+          {
+            from: 'services',
+            allow: ['contracts', 'libs'],
+          },
+        ],
+      },
+    ],
   },
-};
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+    'boundaries/elements': [
+      {
+        type: 'services',
+        pattern: 'services/*',
+      },
+      {
+        type: 'libs',
+        pattern: 'libs/*',
+      },
+      {
+        type: 'contracts',
+        pattern: 'contracts/*',
+      },
+    ],
+  },
+}
